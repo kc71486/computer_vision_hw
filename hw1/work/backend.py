ef6fc73c1405ca9af27f9e78dad3469a71c3ea0d
kc71486 <linda20030102@gmail.com>
2023-10-26 20:42:57 +0800

    add first question bone

---
+++ hw1/work/backend.py
@@ -0,0 +1,24 @@
+import cv2
+
+class cv():
+    def __init__():
+        pass
+
+    def findCorner():
+        pass
+
+    def findIntrinsic():
+        pass
+
+    def findExtrinsic():
+        pass
+
+    def findDistortion():
+        pass
+
+    def showUndistorted():
+        pass
+
+class dl():
+    pass
+
