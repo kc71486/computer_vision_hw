78d80413cb8e31c2023a47e5e2a1ee64b9c0d9c6
kc71486 <linda20030102@gmail.com>
2023-11-01 18:12:47 +0800

    refactor for pycharm

--- hw1/work/backend.py
+++ hw1/work/backend.py
@@ -3,7 +3,7 @@ import os
 import numpy as np
 
 from multimethod import multimethod
-from typing import Any, Union, Callable, Iterable
+from typing import Any, Union
 
 import cv2
 
@@ -11,182 +11,185 @@ import hwutil
 
 os.environ.pop("QT_QPA_PLATFORM_PLUGIN_PATH")
 
-class Assign1():
-    board_corner = (11, 8)
-    imageloader = None
-    __ctr1 = 0
-    __ctr5 = 0
 
-    @multimethod
-    def __init__(self, imageloader:hwutil.ImageLoader):
+class Assign1:
+    board_corner: tuple
+    imageloader: hwutil.ImageLoader
+    __ctr1: int
+    __ctr5: int
+
+    def __init__(self, imageloader: hwutil.ImageLoader):
+        self.board_corner = (11, 8)
         self.imageloader = imageloader
-    
-    @multimethod
+        self.__ctr1 = 0
+        self.__ctr5 = 0
+
     def loop1(self) -> np.ndarray:
-        img = self.findCorner(self.__ctr1)
+        img = self.find_corner(self.__ctr1)
         self.__ctr1 = (self.__ctr1 + 1) % len(self.imageloader)
         return img
-    
-    @multimethod
+
     def loop5(self) -> np.ndarray:
-        img = self.showUndistorted(self.__ctr5)
+        img = self.show_undistorted(self.__ctr5)
         self.__ctr5 = (self.__ctr5 + 1) % len(self.imageloader)
         return img
 
-    @multimethod
-    def findCorner(self, idx:int) -> np.ndarray:
+    def find_corner(self, idx: int) -> np.ndarray:
         img = self.imageloader[idx]
-        findret, corners = cv2.findChessboardCorners(
-                img, self.board_corner, None, None)
-        cv2.drawChessboardCorners(
-                img, self.board_corner, corners, findret)
-        
+        find, corners = cv2.findChessboardCorners(img, self.board_corner)
+        if find:
+            cv2.drawChessboardCorners(img, self.board_corner, corners, find)
+
         img = cv2.resize(img, (512, 512))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return img
 
-    @multimethod
-    def findIntrinsic(self) -> np.ndarray:
+    def find_intrinsic(self) -> np.ndarray:
         imgshape = self.imageloader[0].shape
         cx = self.board_corner[0]
         cy = self.board_corner[1]
 
         # grid, (0,0,0),(0,1,0),(0,2,0)...(1,0,0),(1,1,0)...(m,n,0)
         objpoint = np.zeros((cx * cy, 3), np.float32)
-        objpoint[:,:2] = np.mgrid[0:cx,0:cy].T.reshape(-1,2)
+        objpoint[:, :2] = np.mgrid[0:cx, 0:cy].T.reshape(-1, 2)
         imgpoint = None
 
         objpoints = []
         imgpoints = []
-        
+
         for img in self.imageloader:
-            findret, corners = cv2.findChessboardCorners(
-                img, self.board_corner, None, None)
+            findret, corners = cv2.findChessboardCorners(img, self.board_corner)
             if findret:
                 imgpoints.append(corners)
                 objpoints.append(objpoint)
-        
+
         cammatr = np.identity(3)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-                objpoints, imgpoints, imgshape[0:2], cammatr, None)
+            objpoints, imgpoints, imgshape[0:2], cammatr, None)
 
         return mtx
 
     @multimethod
-    def findExtrinsic(self, idx:int) -> np.ndarray:
+    def findExtrinsic(self, idx: int) -> np.ndarray:
         imgshape = self.imageloader[0].shape
         cx = self.board_corner[0]
         cy = self.board_corner[1]
 
         # grid, (0,0,0),(0,1,0),(0,2,0)...(1,0,0),(1,1,0)...(m,n,0)
         objpoint = np.zeros((cx * cy, 3), np.float32)
-        objpoint[:,:2] = np.mgrid[0:cx,0:cy].T.reshape(-1,2)
+        objpoint[:, :2] = np.mgrid[0:cx, 0:cy].T.reshape(-1, 2)
         imgpoint = None
 
         objpoints = []
         imgpoints = []
-        
+
         for img in self.imageloader:
             findret, corners = cv2.findChessboardCorners(
                 img, self.board_corner, None, None)
             if findret:
                 imgpoints.append(corners)
                 objpoints.append(objpoint)
-        
+
         cammatr = np.identity(3)
         rvecs = np.zeros(3)
         tvecs = np.zeros(3)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-                objpoints, imgpoints, imgshape[0:2], cammatr, None)
-        
+            objpoints, imgpoints, imgshape[0:2], cammatr, None)
+
         rvec = cv2.Rodrigues(rvecs[idx])
         tvec = tvecs[idx]
 
         return np.concatenate((rvec[0], tvec), axis=1)
 
-    @multimethod
-    def findDistortion(self):
+    def find_distortion(self):
         imgshape = self.imageloader[0].shape
         cx = self.board_corner[0]
         cy = self.board_corner[1]
 
         # grid, (0,0,0),(0,1,0),(0,2,0)...(1,0,0),(1,1,0)...(m,n,0)
         objpoint = np.zeros((cx * cy, 3), np.float32)
-        objpoint[:,:2] = np.mgrid[0:cx,0:cy].T.reshape(-1,2)
+        objpoint[:, :2] = np.mgrid[0:cx, 0:cy].T.reshape(-1, 2)
         imgpoint = None
 
         objpoints = []
         imgpoints = []
-        
+
         for img in self.imageloader:
             findret, corners = cv2.findChessboardCorners(
                 img, self.board_corner, None, None)
             if findret:
                 imgpoints.append(corners)
                 objpoints.append(objpoint)
-        
+
         cammatr = np.identity(3)
         rvecs = np.zeros(3)
         tvecs = np.zeros(3)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-                objpoints, imgpoints, imgshape[0:2], cammatr, None)
-        
+            objpoints, imgpoints, imgshape[0:2], cammatr, None)
+
         return dist
 
-    @multimethod
-    def showUndistorted(self, idx:int):
+    def show_undistorted(self, idx: int):
         imgshape = self.imageloader[0].shape
         cx = self.board_corner[0]
         cy = self.board_corner[1]
 
         # grid, (0,0,0),(0,1,0),(0,2,0)...(1,0,0),(1,1,0)...(m,n,0)
         objpoint = np.zeros((cx * cy, 3), np.float32)
-        objpoint[:,:2] = np.mgrid[0:cx,0:cy].T.reshape(-1,2)
+        objpoint[:, :2] = np.mgrid[0:cx, 0:cy].T.reshape(-1, 2)
         imgpoint = None
 
         objpoints = []
         imgpoints = []
-        
+
         for img in self.imageloader:
             findret, corners = cv2.findChessboardCorners(
                 img, self.board_corner, None, None)
             if findret:
                 imgpoints.append(corners)
                 objpoints.append(objpoint)
-        
+
         cammatr = np.identity(3)
         rvecs = np.zeros(3)
         tvecs = np.zeros(3)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-                objpoints, imgpoints, imgshape[0:2], cammatr, None)
-        
+            objpoints, imgpoints, imgshape[0:2], cammatr, None)
+
         img = self.imageloader[idx]
-        
+
         dst = cv2.undistort(img, mtx, dist, None, None)
-        
+
         dst = cv2.resize(dst, (512, 512))
         dst = cv2.cvtColor(dst, cv2.COLOR_BGR2RGB)
-        
+
         return dst
 
-class Assign2():
-    board_corner = (11, 8)
-    fs_path = "../Dataset_CvDl_Hw1/Q2_Image/Q2_lib/alphabet_lib_onboard.txt"
-    imageloader = None
-    intr = None
-    rvecs = []
-    tvecs = []
-    dist = None
-    projection = []
+
+class Assign2:
+    board_corner: tuple[int, int]
+    fs_path: str
+    imageloader: hwutil.ImageLoader
+    intrinsic: Any
+    r_vectors: list
+    t_vectors: list
+    dist: Any
+    projection: list
 
     @multimethod
-    def __init__(self, imageloader:hwutil.ImageLoader):
+    def __init__(self, imageloader: hwutil.ImageLoader):
+        self.board_corner = (11, 8)
+        self.fs_path = "../Dataset_CvDl_Hw1/Q2_Image/Q2_lib/alphabet_lib_onboard.txt"
         self.imageloader = imageloader
-    
+        self.intrinsic = None
+        self.r_vectors = []
+        self.t_vectors = []
+        self.dist = None
+        self.projection = []
+
     @multimethod
     def calcProjection(self):
         imgshape = self.imageloader[0].shape
@@ -195,31 +198,30 @@ class Assign2():
 
         # grid, (0,0,0),(0,1,0),(0,2,0)...(1,0,0),(1,1,0)...(m,n,0)
         objpoint = np.zeros((cx * cy, 3), np.float32)
-        objpoint[:,:2] = np.mgrid[0:cx,0:cy].T.reshape(-1,2)
+        objpoint[:, :2] = np.mgrid[0:cx, 0:cy].T.reshape(-1, 2)
         imgpoint = None
 
         objpoints = []
         imgpoints = []
-        
+
         for img in self.imageloader:
             findret, corners = cv2.findChessboardCorners(
                 img, self.board_corner, None, None)
             if findret:
                 imgpoints.append(corners)
                 objpoints.append(objpoint)
-        
+
         cammatr = np.identity(3)
 
         ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(
-                objpoints, imgpoints, imgshape[0:2], cammatr, None)
+            objpoints, imgpoints, imgshape[0:2], cammatr, None)
 
-        self.intr = mtx
-        self.rvecs = rvecs
-        self.tvecs = tvecs
+        self.intrinsic = mtx
+        self.r_vectors = rvecs
+        self.t_vectors = tvecs
         self.dist = dist
 
-    @multimethod
-    def arBoard(self, idx:int, string:str):
+    def ar_board(self, idx: int, string: str):
         if len(string) > 6:
             raise Exception("string too long")
         if len(string) == 0:
@@ -227,10 +229,10 @@ class Assign2():
 
         self.calcProjection()
         fs = cv2.FileStorage(self.fs_path, cv2.FILE_STORAGE_READ)
-        
+
         basecoord = np.array([[7, 5, 0], [4, 5, 0], [1, 5, 0],
-                            [7, 2, 0], [4, 2, 0], [1, 2, 0]])
-        
+                              [7, 2, 0], [4, 2, 0], [1, 2, 0]])
+
         linestart = []
         lineend = []
         for i in range(len(string)):
@@ -238,14 +240,14 @@ class Assign2():
             for j in range(len(ch)):
                 linestart.append(np.add(ch[j][0], basecoord[i]))
                 lineend.append(np.add(ch[j][1], basecoord[i]))
-        
+
         linestart = np.array(linestart, np.float32)
         lineend = np.array(lineend, np.float32)
 
-        pointstart, _ = cv2.projectPoints(linestart, self.rvecs[idx],
-                self.tvecs[idx], self.intr, self.dist)
-        pointend, _ = cv2.projectPoints(lineend, self.rvecs[idx],
-                self.tvecs[idx], self.intr, self.dist)
+        pointstart, _ = cv2.projectPoints(linestart, self.r_vectors[idx],
+                                          self.t_vectors[idx], self.intrinsic, self.dist)
+        pointend, _ = cv2.projectPoints(lineend, self.r_vectors[idx],
+                                        self.t_vectors[idx], self.intrinsic, self.dist)
 
         img = self.imageloader[idx]
         for i in range(len(linestart)):
@@ -258,83 +260,77 @@ class Assign2():
 
         return img
 
-    @multimethod
-    def arVertical(self, idx:int, string:str):
+    def ar_vertical(self, idx: int, string: str):
         self.calcProjection()
 
-class Assign3():
-    leftwrapper = None
-    rightwrapper = None
 
-    @multimethod
-    def __init__(self, leftwrapper:hwutil.ImageWrapper, rightwrapper:hwutil.ImageWrapper):
-        self.leftwrapper = leftwrapper
-        self.rightwrapper = rightwrapper
+class Assign3:
+    left_wrapper: hwutil.ImageWrapper
+    right_wrapper: hwutil.ImageWrapper
 
-    @multimethod
-    def disparityValue(self, coord:tuple[Union[float,int],Union[float,int]]):
-        outleft = self.leftwrapper.read()
-        outright = self.rightwrapper.read()
+    def __init__(self, leftwrapper: hwutil.ImageWrapper, rightwrapper: hwutil.ImageWrapper):
+        self.left_wrapper = leftwrapper
+        self.right_wrapper = rightwrapper
+
+    def disparity_value(self, coord: tuple[Union[float, int], Union[float, int]]):
+        outleft = self.left_wrapper.read()
+        outright = self.right_wrapper.read()
         left = cv2.cvtColor(outleft, cv2.COLOR_BGR2GRAY)
         right = cv2.cvtColor(outright, cv2.COLOR_BGR2GRAY)
 
         stereo = cv2.StereoBM_create()
         disp = stereo.compute(left, right)
         disparity = np.divide(disp.astype(np.float32), 16).astype(int)
-        
+
         disval = disparity[coord[1]][coord[0]]
         if disval >= 0:
             newcoord = (coord[0] + disval, coord[1])
 
             cv2.circle(outright, newcoord, radius=5,
-                       color=(0,0,255), thickness=-1)
-        
+                       color=(0, 0, 255), thickness=-1)
+
         outleft = cv2.resize(outleft, (512, 512))
         outleft = cv2.cvtColor(outleft, cv2.COLOR_BGR2RGB)
 
         outright = cv2.resize(outright, (512, 512))
         outright = cv2.cvtColor(outright, cv2.COLOR_BGR2RGB)
-        
+
         disp = np.maximum(disp, 0)
         disp = cv2.resize(disp, (512, 512))
-        disp = cv2.merge((disp,disp,disp))
+        disp = cv2.merge((disp, disp, disp))
 
         return outleft, outright, disp
 
-class Assign4():
 
-    @multimethod
-    def __init__(self, leftwrapper, rightwrapper):
-        self.leftwrapper = leftwrapper
-        self.rightwrapper = rightwrapper
+class Assign4:
+    left_wrapper: hwutil.ImageWrapper
+    right_wrapper: hwutil.ImageWrapper
 
-    @multimethod
+    def __init__(self, left_wrapper, right_wrapper):
+        self.left_wrapper = left_wrapper
+        self.right_wrapper = right_wrapper
+
     def siftKeypoint(self):
         pass
-    
-    @multimethod
+
     def siftMatch(self):
         pass
 
-class Assign5():
 
-    @multimethod
-    def __init__(self, leftwrapper, rightwrapper):
-        self.leftwrapper = leftwrapper
-        self.rightwrapper = rightwrapper
+class Assign5:
+    wrapper: hwutil.ImageWrapper
 
-    @multimethod
+    def __init__(self, wrapper):
+        self.left_wrapper = wrapper
+
     def showAugment(self):
         pass
 
-    @multimethod
     def showModel(self):
         pass
 
-    @multimethod
     def showAccuracyLoss(self):
         pass
-    
-    @multimethod
+
     def predictLabel(self):
         pass
--- hw1/work/hwutil.py
+++ hw1/work/hwutil.py
@@ -3,93 +3,89 @@ import os
 import time
 import threading
 
-from multimethod import multimethod
-from typing import Any, Union, Callable, Iterable
+import numpy as np
+
+from typing import Callable
+from typing_extensions import Self
 
 import cv2
 
-class ImageLoader():
-    files = None
-    lastupdated = -1
-    __idx = -1
 
-    @multimethod
+class ImageLoader:
+    files: list[str]
+    last_updated: float
+    __idx: int
+
     def __init__(self):
-        pass
+        self.files = []
+        self.last_updated = -1
+        self.__idx = -1
 
-    @multimethod
     def __len__(self):
         if self.files is None:
             return 0
         return len(self.files)
 
-    @multimethod
-    def __getitem__(self, index:int):
+    def __getitem__(self, index: int) -> np.ndarray:
         maxsize = len(self.files)
         if index < 0 or index >= maxsize:
             raise IndexError
         return cv2.imread(self.files[index])
 
-    @multimethod
-    def __iter__(self):
+    def __iter__(self) -> Self:
         self.__idx = -1
         return self
 
-    @multimethod
-    def __next__(self):
+    def __next__(self) -> np.ndarray:
         self.__idx += 1
         if self.__idx < len(self.files):
             return cv2.imread(self.files[self.__idx])
         else:
             raise StopIteration
 
-    @multimethod
-    def setPath(self, folder:str):
+    def set_path(self, folder: str) -> None:
         files = []
         for file in os.listdir(folder):
             fullpath = os.path.join(folder, file)
             if os.path.isfile(fullpath):
                 files.append(fullpath)
         self.files = files
-        lastupdated = time.time()
-       
-class ImageWrapper():
-    file = None
-    lastupdated = -1
+        self.last_updated = time.time()
 
-    @multimethod
-    def __init__(self):
-        pass
-    
-    @multimethod
-    def setPath(self, file: str):
+
+class ImageWrapper:
+    file: str
+    last_updated: float
+
+    def __init__(self) -> None:
+        self.file = ""
+        self.last_updated = -1
+
+    def set_path(self, file: str) -> None:
         self.file = file
-        lastupdated = time.time()
-   
-    @multimethod
-    def read(self):
+        self.last_updated = time.time()
+
+    def read(self) -> np.ndarray:
         return cv2.imread(self.file)
 
-class SetInterval():
-    interval = None
-    action = None
-    stopEvent = None
 
-    @multimethod
-    def __init__(self, interval:Union[float, int], action:Callable):
+class SetInterval:
+    interval: float
+    action: Callable
+    stopEvent: threading.Event
+
+    def __init__(self, interval: float | int, action: Callable) -> None:
         self.interval = interval
         self.action = action
         self.stopEvent = threading.Event()
-        thread = threading.Thread(target=self.__setInterval)
+        thread = threading.Thread(target=self.__set_interval)
         thread.start()
-    
-    @multimethod
-    def __setInterval(self):
-        nextTime = time.time() + self.interval
-        while not self.stopEvent.wait(nextTime - time.time()):
-            nextTime += self.interval
+
+    def __set_interval(self) -> None:
+        next_time = time.time() + self.interval
+        while not self.stopEvent.wait(next_time - time.time()):
+            next_time += self.interval
             self.action()
-    
-    @multimethod
-    def cancel(self):
+
+    def cancel(self) -> None:
         self.stopEvent.set()
