5c1b3b2de0dc0a85878ef401ac189c510f01abb6
kc71486 <linda20030102@gmail.com>
2023-12-10 13:59:23 +0800

    finalize shell

---
+++ hw1/runmain.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source hw1_env/bin/activate
+cd work
+python3 main.py
+deactivate
--- hw1/startup.sh
+++ hw1/startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 #prelimary:
-sudo apt install python3 pip
+sudo apt install python3 python3-pip python3.10-venv
+
+python3 -m venv hw1_env
+
+source hw1_env/bin/activate
 pip install matplotlib
-pip install multimethod
 pip install pyqt5
 pip install opencv-contrib-python
 pip install torch
+pip install torchvision
 pip install torchinfo
+deactivate
