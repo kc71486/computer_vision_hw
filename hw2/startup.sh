d320a7d895158e6d18ef98550545016b2441790a
kc71486 <linda20030102@gmail.com>
2024-01-05 14:50:36 +0800

    update

--- hw2/startup.sh
+++ hw2/startup.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-#prelimary:
-sudo apt install python3 python3-pip python3.10-venv
-sudo apt install ffmpeg
+pac_list=("python3.10" "python3-pip" "python3.10-venv", "ffmpeg")
+env_name="hw2_env"
 
-python3 -m venv hw2_env
+for pac in ${pac_list[@]}
+do
+    if [ "$(dpkg -l | awk '/'$pac'/ {print }' | wc -l)" -eq 0 ]; then
+        sudo apt install $pac
+    fi
+done
+
+if [ ! -d $env_name ]
+then
+    python3 -m venv $env_name
+    echo "created environment"
+fi
 
 source hw2_env/bin/activate
 pip install matplotlib
